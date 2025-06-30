72eaf1afc243fae4fcc5ac3710adc6aa740fa6de
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-06-30 16:27:37 +0545

    changing the ui in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/movie_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Fetch movies when screen loads
+    Provider.of<MovieProvider>(context, listen: false).fetchNowPlayingMovies();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final movieProvider = Provider.of<MovieProvider>(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Movie Sansaar')),
-      body: const Center(child: Text('Welcome to Movie Sansaar!')),
+      body: movieProvider.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: movieProvider.nowPlayingMovies.length,
+              itemBuilder: (context, index) {
+                final movie = movieProvider.nowPlayingMovies[index];
+                return ListTile(
+                  leading: Image.network(
+                    'https://image.tmdb.org/t/p/w92${movie.posterPath}',
+                    width: 50,
+                    fit: BoxFit.cover,
+                    errorBuilder: (_, __, ___) => const Icon(Icons.movie),
+                  ),
+                  title: Text(movie.title),
+                  subtitle: Text(movie.releaseDate),
+                );
+              },
+            ),
     );
   }
 }
