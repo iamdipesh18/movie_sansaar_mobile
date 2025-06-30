e3cb8674984988a32c3d4215ec0d9d0ea3fdd1fa
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-06-30 12:42:51 +0545

    initial commit and  project folder structuring

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieSansaarApp());
+}
+
+class MovieSansaarApp extends StatelessWidget {
+  const MovieSansaarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Sansaar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Center(child: Text('Hello, Movie Sansaar!'))),
+    );
+  }
+}
